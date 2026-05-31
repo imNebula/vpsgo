502651f7c9d6fd415cfc07f649b84e60f8c713e9
imNebula <242760680+imNebula@users.noreply.github.com>
2026-05-31 19:50:48 +0800

    Update

--- vpsgo.sh
+++ vpsgo.sh
@@ -38,7 +38,7 @@ fi
 
 set -uo pipefail
 
-VERSION="4.14"
+VERSION="4.15"
 # --- 全局变量 ---
 SCRIPT_DIR="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
 INSTALL_PATH="${VPSGO_INSTALL_PATH:-/usr/local/bin/vpsgo}"
